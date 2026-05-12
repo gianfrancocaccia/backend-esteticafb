7dde81ffc6c771367e06459065104e1d7ee3ae68
Gianfranco Caccia <gianfrancocaccia@hotmail.com>
2026-05-12 10:49:53 -0300

    kikil

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONT_URL,
-    methods: ["GET", "POST", "OPTIONS"]
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]  // ← esto faltaba
   })
 );
 
