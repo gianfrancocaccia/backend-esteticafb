9fffc4fa7440e440edeabbc6210fffbdac15929a
Gianfranco Caccia <gianfrancocaccia@hotmail.com>
2026-05-12 08:19:29 -0300

    backed

---
+++ server.js
@@ -0,0 +1,94 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import {
+  MercadoPagoConfig,
+  Preference
+} from "mercadopago";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN
+});
+
+app.post(
+  "/create_preference",
+  async (req, res) => {
+
+    try {
+
+      const preference =
+        new Preference(client);
+
+      const response =
+        await preference.create({
+
+          body: {
+
+            items: [
+              {
+                title:
+                  req.body.title,
+
+                quantity: 1,
+
+                unit_price:
+                  Number(
+                    req.body.price
+                  ),
+
+                currency_id: "ARS"
+              }
+            ],
+
+            back_urls: {
+
+              success:
+`${process.env.FRONT_URL}/gracias`
+
+            }
+
+          }
+
+        });
+
+      res.json({
+
+        id: response.id,
+
+        init_point:
+          response.init_point
+
+      });
+
+    } catch (error) {
+
+      console.log(error);
+
+      res.status(500).json({
+
+        error:
+          "Error creando preferencia"
+
+      });
+
+    }
+
+  }
+);
+
+app.listen(3000, () => {
+
+  console.log(
+    "Servidor corriendo en puerto 3000"
+  );
+
+});
